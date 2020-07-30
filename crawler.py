9c3e1c90e1b3f99cb552a20e14e1cabfa7485ff3
ishmeetsingh217 <ishmeetsingh217@gmail.com>
2020-07-30 11:58:40 -0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+
 import re
 from html.parser import HTMLParser
 from urllib.request import urlopen
@@ -15,6 +16,8 @@ if not os.path.exists(dataset_path):
     os.mkdir(dataset_path)
 
 # Get data from HTML page
+
+
 class LinkParser(HTMLParser):
 
     def get_links(self, url):
@@ -30,26 +33,34 @@ class LinkParser(HTMLParser):
         return html_string, self.links
 
 # Remove substring between from start to end
+
+
 def remove_between(document, start_str, end_str):
 
     while start_str in document and end_str in document:
         # Get start and end index
         start_str_index = document.find(start_str)
-        end_str_index = document.find(end_str, start_str_index) + len(end_str) - 1
+        end_str_index = document.find(
+            end_str, start_str_index) + len(end_str) - 1
         # get new content without from start to end
-        document = document[0: start_str_index] + document[end_str_index + 1: len(document)]
+        document = document[0: start_str_index] + \
+            document[end_str_index + 1: len(document)]
     return document
 
 # Get substring between from first to last
-def find_between_r( s, first, last ):
+
+
+def find_between_r(s, first, last):
     try:
-        start = s.rindex( first ) + len( first )
-        end = s.rindex( last, start )
+        start = s.rindex(first) + len(first)
+        end = s.rindex(last, start)
         return s[start:end]
     except ValueError:
         return ""
 
 # Function that get content of web page
+
+
 def parsePage(website_url):
 
     parser = LinkParser()
@@ -63,22 +74,23 @@ def parsePage(website_url):
             data, links = parser.get_links(website_url)
             loop_data = False
         except:
-            return [],"",False
+            return [], "", False
     # Get name of files
     if "title" in str(data):
-        name = str(find_between_r(str(data),"<title>","</title>")) + ".txt"
+        name = str(find_between_r(str(data), "<title>", "</title>")) + ".txt"
         if "|" in name:
-            name = name.replace("|","")
+            name = name.replace("|", "")
         elif ":" in name:
-            name = name.replace(":","")
+            name = name.replace(":", "")
         elif ";" in name:
-            name = name.replace(";","")
+            name = name.replace(";", "")
         elif "&" in name:
-            name = name.replace("&","")
+            name = name.replace("&", "")
         else:
             pass
     else:
-        name = str(website_url).split("/")[-2] + "_" + str(website_url).split("/")[-1]
+        name = str(website_url).split(
+            "/")[-2] + "_" + str(website_url).split("/")[-1]
         if "_" == name[-1]:
             name = name[:-1] + ".txt"
         else:
@@ -104,7 +116,7 @@ def parsePage(website_url):
     reallines = []
     lines = str(data).split(" ")
     for line in lines:
-        if str(line) == "" or "" ==  str(line).strip(" "):
+        if str(line) == "" or "" == str(line).strip(" "):
             continue
         else:
             reallines.append(line)
@@ -115,7 +127,7 @@ def parsePage(website_url):
         if "&" in line:
             continue
         elif "\t" in line:
-            line = line.replace("\t","")
+            line = line.replace("\t", "")
         elif "," == line or '"' == line or ":" == line or "," == line or ")" == line or "(" == line or "^" == line or "|" == line:
             continue
         elif len(factlines) > 500:
@@ -123,10 +135,12 @@ def parsePage(website_url):
         factlines.append(line)
     if len(factlines) < 200:
         flag = False
-    return factlines,name,flag
+    return factlines, name, flag
 
 # function that out docs
-def putfile(topic_word,urls):
+
+
+def putfile(topic_word, urls):
 
     # Get topic path and make topic folder
     topic_path = os.path.join(dataset_path, topic_word)
@@ -137,16 +151,18 @@ def putfile(topic_word,urls):
     file_index = 0
     for url in urls:
         if "(" in str(url):
-            url = remove_between(str(url),"(",")")
+            url = remove_between(str(url), "(", ")")
         print(url)
         # Get data from HTML page
-        lines,name,flag = parsePage(url)
+        lines, name, flag = parsePage(url)
         if not flag:
             continue
         elif k > 11:
             break
         # Get txt file path
-        content_path = os.path.join(topic_path,name)
+        if "/" in name:
+            name = name.replace("/", "")
+        content_path = os.path.join(topic_path, name)
         if os.path.isfile(content_path):
             file_index = file_index + 1
             content_path = os.path.join(topic_path, str(file_index) + name)
@@ -156,7 +172,7 @@ def putfile(topic_word,urls):
         array_index = 0
         line_index = 1
         # write the content and make the txt file
-        with open(content_path, "w",encoding="utf-8") as file:
+        with open(content_path, "w", encoding="utf-8") as file:
             for line in lines:
                 while True:
                     try:
@@ -169,6 +185,7 @@ def putfile(topic_word,urls):
                         break
             file.close()
         k = k + 1
+
 
 def main():
 
@@ -192,10 +209,9 @@ def main():
                 if "youtube" in str(i):
                     continue
                 my_results_list.append(i)
-            putfile(topic,my_results_list)
+            putfile(topic, my_results_list)
         except Exception as error:
-            print("First step :",error)
+            print("First step :", error)
+
 
 main()
-
-
